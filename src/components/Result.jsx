57e2dd2c8626893797ab6367c7f48523d2185cf5
Fithamlak <justiceofgod01@gmail.com>
2023-02-21 22:26:18 +0300

    Creat Result desplay section in a separate file and import into calculator.js

---
+++ src/components/Result.jsx
@@ -0,0 +1,9 @@
+import '../css/result.css';
+
+function Result() {
+  return (
+    <textarea className="result">0</textarea>
+  );
+}
+
+export default Result;
--- src/components/calculator.jsx
+++ src/components/calculator.jsx
@@ -1,40 +1,44 @@
 import '../css/calculator.css';
+import Result from './Result';
 
 function Calculator() {
   return (
-    <table>
-      <tbody>
-        <tr>
-          <td>AC</td>
-          <td>+/-</td>
-          <td>%</td>
-          <td className="oprator">/</td>
-        </tr>
-        <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td className="oprator">x</td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-          <td className="oprator">-</td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td className="oprator">+</td>
-        </tr>
-        <tr>
-          <td colSpan={2} className="bottom">0</td>
-          <td className="bottom">.</td>
-          <td className="oprator">=</td>
-        </tr>
-      </tbody>
-    </table>
+    <div>
+      <Result />
+      <table>
+        <tbody>
+          <tr>
+            <td>AC</td>
+            <td>+/-</td>
+            <td>%</td>
+            <td className="oprator">/</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td className="oprator">x</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td className="oprator">-</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td className="oprator">+</td>
+          </tr>
+          <tr>
+            <td colSpan={2} className="bottom">0</td>
+            <td className="bottom">.</td>
+            <td className="oprator">=</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
