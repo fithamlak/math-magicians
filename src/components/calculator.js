fb35e65ffaaf29e26e3e3d2c9265ebd2146176dd
Fithamlak <justiceofgod01@gmail.com>
2023-02-21 21:22:41 +0300

    Create colculator.js inside components folder

---
+++ src/components/calculator.js
@@ -0,0 +1,39 @@
+function Calculator() {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>AC</td>
+          <td>+/-</td>
+          <td>%</td>
+          <td>/</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+          <td>x</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>+</td>
+        </tr>
+        <tr>
+          <td>0</td>
+          <td>.</td>
+          <td>=</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Calculator;
