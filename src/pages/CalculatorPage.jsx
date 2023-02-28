3a703d24c287f9ca8ee6d1436fac727f1b58afdf
Fithamlak <justiceofgod01@gmail.com>
2023-02-28 22:52:19 +0300

    Add Calculator Page

---
+++ src/pages/CalculatorPage.jsx
@@ -0,0 +1,10 @@
+import Calculator from '../components/Calculator';
+
+const CalculatorPage = () => (
+  <section className="calculator">
+    <h2>Lets do some math!</h2>
+    <Calculator />
+  </section>
+);
+
+export default CalculatorPage;
