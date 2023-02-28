1d1134b0ddb36a63ee058d23c2227bff7e87cf0f
Fithamlak <justiceofgod01@gmail.com>
2023-02-28 23:21:14 +0300

    Finish Importing All Pages into App Using Routes, Rout And Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import Calculator from './components/calculator';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Calculator from './pages/CalculatorPage';
+import Quote from './pages/Quotes';
 import './css/app.css';
 
 function App() {
   return (
     <div className="main-container">
-      <Calculator />
+      <Routes path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Routes>
     </div>
   );
 }
