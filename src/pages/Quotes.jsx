ab5fa17659fcd3020c92d4e7f31b1123454ef7b5
Fithamlak <justiceofgod01@gmail.com>
2023-02-28 23:06:22 +0300

    Add Style For Quotes Page

--- src/pages/Quotes.jsx
+++ src/pages/Quotes.jsx
@@ -1,3 +1,5 @@
+import '../css/quote.css';
+
 const Quote = () => (
   <h1 className="quote">Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding - William Paul Thurston</h1>
 );
