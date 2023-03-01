2b9f504a2d5bf0957263c6d4678d9f97ad62c606
Fithamlak <justiceofgod01@gmail.com>
2023-03-01 10:42:08 +0300

    Update Navbar.jsx
    give link class for navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => (
   <nav className="navbar">
     <h1 className="main-header">Math Magicians</h1>
     <ul className="navLinks">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="calculator">Calculator</NavLink>
-      <NavLink to="quote">Quote</NavLink>
+      <NavLink to="/" className="link">Home</NavLink>
+      <NavLink to="calculator" className="link">Calculator</NavLink>
+      <NavLink to="quote" className="link">Quote</NavLink>
     </ul>
   </nav>
 );
